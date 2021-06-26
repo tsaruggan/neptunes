98d546ba89db988e79f0f70e04d5e977f7a7d556
Saruggan Thiruchelvan <tsaruggan@gmail.com>
2021-06-26 12:57:04 -0400

    Initial Commit

---
+++ Apollo/ApolloApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApolloApp.swift
+//  Apollo
+//
+//  Created by Saruggan Thiruchelvan on 2021-06-26.
+//
+
+import SwiftUI
+
+@main
+struct ApolloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
