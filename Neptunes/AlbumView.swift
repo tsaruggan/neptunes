30855328dcab69db5aae5de4f950e62129af1f79
Saruggan Thiruchelvan <tsaruggan@gmail.com>
2021-07-05 19:51:57 -0400

    added tracklist to AlbumView

--- Neptunes/AlbumView.swift
+++ Neptunes/AlbumView.swift
@@ -21,16 +21,11 @@ struct AlbumView: View {
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             ZStack {
-                
-                
-                
                 VStack {
                     StickyHeaderView(header: album.header)
                     Spacer()
                 }
                 
-                
-    
                 VStack {
                     Image(album.image)
                         .resizable()
@@ -38,7 +33,7 @@ struct AlbumView: View {
                         .cornerRadius(8)
                         .padding(.horizontal, 88)
                         .padding(.top, 100)
-                    Spacer(minLength: 20)
+                    Spacer()
                     VStack(alignment: .leading, spacing: 0) {
                         Text(album.isSingle ? "Single" : "Album")
                             .font(.subheadline)
@@ -46,7 +41,7 @@ struct AlbumView: View {
                         Text(album.title)
                             .fontWeight(.bold)
                             .font(.title)
-                            .padding(.bottom, 10)
+                            .padding(.bottom, 4)
                         HStack {
                             Image(album.artist.image)
                                 .resizable()
@@ -57,8 +52,31 @@ struct AlbumView: View {
                         }
                     }
                     .frame(width: 320, alignment: .leading)
+                    
+                    VStack {
+                        ForEach(album.songs.indices) { i in
+                            HStack(spacing: 14) {
+                                Text(i+1 < 10 ? String(format: "%2d ", i+1) : String(i+1))
+                                    .monospacedDigit()
+                                    .foregroundColor(.secondary)
+                                Text(album.songs[i].title)
+                                    .fontWeight(.medium)
+                                if album.songs[i].isExplicit {
+                                    Image(systemName: "e.square.fill")
+                                        .foregroundColor(.red)
+                                }
+                                Spacer(minLength: 0)
+                                Image(systemName: "ellipsis")
+                            }
+                            .padding(12)
+                            .background(.ultraThinMaterial)
+                            .clipShape(RoundedRectangle(cornerRadius:8))
+
+                        }
+                    }
+                    .padding(20)
+                    Spacer()
                 }
-                                
             }
         }
         .edgesIgnoringSafeArea(.top)
@@ -98,16 +116,8 @@ struct ToolbarButtonStyle: ButtonStyle {
             .frame(width: width, height: width)
             .buttonStyle(.bordered)
             .foregroundColor(.primary)
-            .background(Color.red.opacity(0.2), in: Circle())
+        //            .background(Color.red.opacity(0.2), in: Circle())
             .background(.ultraThinMaterial, in: Circle())
-    }
-}
-
-struct AlbumView_Previews: PreviewProvider {
-    static var previews: some View {
-        NavigationView{
-            AlbumView(album: MusicModel().albums[2])
-        }
     }
 }
 
@@ -126,6 +136,14 @@ struct StickyHeaderView: View {
                            : UIScreen.main.bounds.width / 3)
             }
             .frame(height: UIScreen.main.bounds.width / 3, alignment: .center)
+        }
+    }
+}
+
+struct AlbumView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+            AlbumView(album: MusicModel().albums[1])
         }
     }
 }
--- Neptunes/MusicModel.swift
+++ Neptunes/MusicModel.swift
@@ -42,6 +42,7 @@ struct Song: Sortable, Identifiable {
         if let artist = album?.artist { return artist }
         return nil
     }
+    var isExplicit: Bool = false
     var id = UUID()
 }
 
@@ -98,25 +99,25 @@ struct MusicModel {
             Album(title: "Days Before Rodeo", artist: self.artists[2], image: "travis_scott_album_art_2", header: "travis_scott_header_art"),
         ]
         self.albums[0].addSongs(
-            Song(title: "Not Around")
+            Song(title: "Not Around", isExplicit: true)
         )
         self.albums[1].addSongs(
-            Song(title: "At Your Best (You Are Love"),
+            Song(title: "At Your Best (You Are Love)"),
             Song(title: "Alabama"),
-            Song(title: "Mine"),
-            Song(title: "U-N-I-T-Y"),
+            Song(title: "Mine", isExplicit: true),
+            Song(title: "U-N-I-T-Y", isExplicit: true),
             Song(title: "Ambience 001 (A Certain Way)"),
             Song(title: "Comme des Garçons"),
             Song(title: "Xenons"),
             Song(title: "Ambience 002 (Honeybaby)"),
-            Song(title: "Wither"),
+            Song(title: "Wither", isExplicit: true),
             Song(title: "Hublots"),
             Song(title: "In Here Somewhere"),
             Song(title: "Slide on Me"),
             Song(title: "Sideways"),
             Song(title: "Florida"),
             Song(title: "Impietas + Deathwish (ASR)"),
-            Song(title: "Rushes"),
+            Song(title: "Rushes", isExplicit: true),
             Song(title: "Rushes To"),
             Song(title: "Higgs"),
             Song(title: "Mitsubishi Sony"),
@@ -124,15 +125,15 @@ struct MusicModel {
         )
         self.albums[2].addSongs(
             Song(title: "Days Before Rodeo: The Prayer"),
-            Song(title: "Mamacita (feat. Rich Homie Quan & Young Thug)"),
-            Song(title: "Quintana Pt. 2"),
+            Song(title: "Mamacita (feat. Rich Homie Quan & Young Thug)", isExplicit: true),
+            Song(title: "Quintana Pt. 2", isExplicit: true),
             Song(title: "Drugs You Should Try It"),
             Song(title: "Don't Play (feat. The 1975 & Big Sean)"),
             Song(title: "Skyfall (feat. Young Thug)"),
-            Song(title: "Zonbies"),
+            Song(title: "Zonbies", isExplicit: true),
             Song(title: "Sloppy Toppy (feat. Migos & Peewee Longway)"),
-            Song(title: "Basement Freestyle"),
-            Song(title: "Backyard"),
+            Song(title: "Basement Freestyle", isExplicit: true),
+            Song(title: "Backyard", isExplicit: true),
             Song(title: "Grey"),
             Song(title: "BACC")
         )
