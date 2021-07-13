3f94fbe01b4ec6f3447b3b657cf7154da8526453
Saruggan Thiruchelvan <tsaruggan@gmail.com>
2021-07-12 23:43:19 -0400

    changed ColorAnalyzer to use UIColor instead of SwiftUI.Color

--- Neptunes/AlbumView/AlbumViewModel.swift
+++ Neptunes/AlbumView/AlbumViewModel.swift
@@ -16,13 +16,12 @@ final class AlbumViewModel: ObservableObject {
     
     init(album: Album) {
         self.album = album
-        self.colors = ColorAnalyzer.getColors(albumArt: album.image, headerArt: album.header!, 3)
         self.palette = Palette(
             primary: (light: .black, dark: .white),
             secondary: (light: .blue, dark: .teal),
             tertiary: (light: .green, dark: .mint),
             accent: (light: .indigo, dark: .purple),
-            background: (light: .white, dark: .black)
+            background: (light: .blue, dark: .blue)
         )
     }
 }
--- Neptunes/Models/Color/ColorAnalyzer.swift
+++ Neptunes/Models/Color/ColorAnalyzer.swift
@@ -12,7 +12,7 @@ import SwiftUI
 
 class ColorAnalyzer {
     
-    private static func getPixelsFromImage(cgImage: CGImage, _ resizedWidth: Int, _ resizedHeight: Int) -> [Vector] {
+    static func getPixelsFromImage(cgImage: CGImage, _ resizedWidth: Int, _ resizedHeight: Int) -> [Vector] {
         let image = SwiftImage.Image<RGBA<UInt8>>(cgImage: cgImage)
         let resizedImage = image.resizedTo(width: resizedWidth, height: resizedHeight, interpolatedBy: .nearestNeighbor)
         let pixels: [Vector] = resizedImage.map { pixel in
@@ -21,30 +21,37 @@ class ColorAnalyzer {
         return pixels
     }
     
-    private static func colorfulness(_ r: Double, _ g: Double, _ b: Double) -> Double {
-        let rg = abs(r - g)
-        let yb = abs(0.5 * (r + g) - b)
+    static func colorfulness(_ color: UIColor) -> Double {
+        var red: CGFloat        = 0.0
+        var green: CGFloat      = 0.0
+        var blue: CGFloat       = 0.0
+        var alpha: CGFloat      = 0.0
+        color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        let rg = abs(red - green)
+        let yb = abs(0.5 * (red + green) - blue)
         let root = (pow(rg, 2) + pow(yb, 2)).squareRoot()
         return root * 0.3
     }
     
-    static func getColors(albumArt: String, headerArt: String, _ numColors: Int) -> [Color] {
+    static func getColors(albumArt: String, headerArt: String, _ numColors: Int) -> [UIColor] {
         let albumImage: CGImage = UIImage(named: albumArt)!.cgImage!
         let headerImage: CGImage = UIImage(named: headerArt)!.cgImage!
+        
         let pixels = getPixelsFromImage(cgImage: albumImage, 16, 16) + getPixelsFromImage(cgImage: headerImage, 48, 16)
         let kmm = KMeans<Int>(labels: Array(1...numColors))
         kmm.trainCenters(pixels, convergeDistance: 0.01)
-        let centroids = kmm.centroids.sorted { c1, c2 in
-            return colorfulness(c1.data[0], c1.data[1], c1.data[2])
-            > colorfulness(c2.data[0], c2.data[1], c2.data[2])
+
+        let colors = kmm.centroids.map { centroid in
+            return UIColor(red: centroid.data[0] / 255.0,
+                           green: centroid.data[1] / 255.0,
+                           blue: centroid.data[2] / 255.0,
+                           alpha: 1.0
+            )
         }
-        let colors = centroids.map { centroid in
-                return Color(red: centroid.data[0] / 255.0,
-                               green: centroid.data[1] / 255.0,
-                               blue: centroid.data[2] / 255.0)
-        }
-        return colors
+        let sortedColors = colors.sorted { colorfulness($0) > colorfulness($1) }
+        return sortedColors
     }
+    
 }
 
 extension RGBA where Channel == UInt8 {
@@ -52,4 +59,17 @@ extension RGBA where Channel == UInt8 {
     var greenDouble: Double { return Double(green) }
     var blueDouble: Double { return Double(blue) }
     var alphaDouble: Double { return Double(alpha) }
+}
+
+extension UIColor {
+    func adjustBrightness(_ newBrightness: CGFloat) -> UIColor {
+        var hue: CGFloat        = 0.0
+        var saturation: CGFloat = 0.0
+        var brightness: CGFloat = 0.0
+        var alpha: CGFloat      = 0.0
+        
+        self.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha)
+        let newColor = UIColor( hue: hue, saturation: saturation, brightness: newBrightness, alpha: alpha)
+        return newColor
+    }
 }
