1a89f6b233fa65f256c54d8ad73847741cfd17c0
Saruggan Thiruchelvan <tsaruggan@gmail.com>
2021-07-18 18:29:50 -0400

    added scrubber handle at given percent in ScrubberView

--- Neptunes/PlayerView/ScrubberView.swift
+++ Neptunes/PlayerView/ScrubberView.swift
@@ -8,10 +8,29 @@
 import SwiftUI
 
 struct ScrubberView: View {
+    var path: some View {
+        GeometryReader { geometry in
+            let startPoint: CGPoint = CGPoint(x: geometry.frame(in: .local).minX, y: geometry.frame(in: .local).maxY)
+            let endPoint: CGPoint = CGPoint(x: geometry.frame(in: .local).maxX, y: geometry.frame(in: .local).maxY)
+            let controlPoint: CGPoint = CGPoint(x: geometry.frame(in: .local).midX, y: geometry.frame(in: .local).minY)
+            ZStack{
+                QuadBezier(startPoint: startPoint, endPoint: endPoint, controlPoint: controlPoint)
+                    .stroke(Color.teal, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+                let percent = 0.69
+                Circle()
+                    .frame(width: 24, height: 24)
+                    .position(
+                        x:  QuadBezier(startPoint: startPoint, endPoint: endPoint, controlPoint: controlPoint).point(at: percent).x,
+                        y:  QuadBezier(startPoint: startPoint, endPoint: endPoint, controlPoint: controlPoint).point(at: percent).y
+                    )
+            }
+            
+        }
+        .frame(width: 200, height: 50)
+    }
     var body: some View {
-        QuadBezier()
-            .stroke(Color.teal, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
-            .frame(width: 200, height: 50)
+        path
+        
     }
 }
 
@@ -22,13 +41,22 @@ struct ScrubberView_Previews: PreviewProvider {
 }
 
 struct QuadBezier: Shape {
+    var startPoint: CGPoint
+    var endPoint: CGPoint
+    var controlPoint: CGPoint
     func path(in rect: CGRect) -> Path {
         var path = Path()
-        let startPoint: CGPoint = CGPoint(x: rect.minX, y: rect.maxY)
-        let endPoint: CGPoint = CGPoint(x: rect.maxX, y: rect.maxY)
-        let controlPoint: CGPoint = CGPoint(x: rect.midX, y: rect.minY)
+        
         path.move(to: startPoint)
         path.addQuadCurve(to: endPoint, control: controlPoint)
         return path
+    }
+    
+    func point(at t: CGFloat) -> CGPoint {
+        let t1 = 1 - t
+        return CGPoint(
+            x: t1 * t1 * startPoint.x + 2 * t * t1 * controlPoint.x + t * t * endPoint.x,
+            y: t1 * t1 * startPoint.y + 2 * t * t1 * controlPoint.y + t * t * endPoint.y
+        )
     }
 }
