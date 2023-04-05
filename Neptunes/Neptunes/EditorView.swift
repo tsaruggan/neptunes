39b1645d47a626c957f26ae11ea3171d9b1b7e96
Saruggan Thiruchelvan <tsaruggan@gmail.com>
2023-04-05 14:23:28 -0400

    yuh

--- Neptunes/Neptunes/EditorView.swift
+++ Neptunes/Neptunes/EditorView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct EditorView: View {
     
@@ -13,44 +14,75 @@ struct EditorView: View {
     
     @Binding var presentingEditor: Bool
     
+    @State private var selectedItem: PhotosPickerItem? = nil
+    
     @Environment(\.managedObjectContext) private var viewContext
     var fileManager = LocalFileManager()
     
     var body: some View {
+        
         Form {
             Section(header: Text("Metadata")) {
                 TextField("Title", text: $viewModel.currentTitle)
                 TextField("Album", text: $viewModel.currentAlbumName)
                 TextField("Artist", text: $viewModel.currentArtist)
             }
-
-            Button("Submit") {
-                let newArtist = Artist(context: viewContext)
-                newArtist.title = viewModel.currentArtist
-                
-                let newAlbum = Album(context: viewContext)
-                newAlbum.title = viewModel.currentAlbumName
-                newAlbum.artist = newArtist
-                newAlbum.coverArtwork = viewModel.currentArtwork
-                
-                let newSong = Song(context: viewContext)
-                newSong.title = viewModel.currentTitle
-                newSong.album = newAlbum
-                newSong.artist = newArtist
-                newSong.id = UUID()
-                
-                fileManager.saveSongFromURL(url: viewModel.currentURL!, song: newSong)
-                presentingEditor = false
-                
-                do {
-                    try viewContext.save()
-                } catch {
-                    // Replace this implementation with code to handle the error appropriately.
-                    // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                    let nsError = error as NSError
-                    fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            
+            Section(header: Text("Cover Artwork")) {
+                PhotosPicker(selection: $selectedItem, matching: .images) {
+                    if let artwork = viewModel.currentArtwork, let image = UIImage(data: artwork) {
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                    } else {
+                        Image("defaultcover")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                    }
+                }
+                .onChange(of: selectedItem) { newItem in
+                    Task {
+                        if let data = try? await newItem?.loadTransferable(type: Data.self) {
+                            viewModel.currentArtwork = data
+                        }
+                    }
                 }
             }
+            
+            Button("Add song to library") {
+                addSong()
+                presentingEditor = false
+            }
+            
+        }
+    }
+    
+    func addSong() {
+        let newArtist = Artist(context: viewContext)
+        newArtist.title = viewModel.currentArtist
+        
+        let newAlbum = Album(context: viewContext)
+        newAlbum.title = viewModel.currentAlbumName
+        newAlbum.artist = newArtist
+        newAlbum.coverArtwork = viewModel.currentArtwork
+        
+        let newSong = Song(context: viewContext)
+        newSong.title = viewModel.currentTitle
+        newSong.album = newAlbum
+        newSong.artist = newArtist
+        newSong.id = UUID()
+        
+        fileManager.saveSongFromURL(url: viewModel.currentURL!, song: newSong)
+        
+        do {
+            try viewContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
     }
 }
@@ -61,7 +93,8 @@ final class EditorViewModel: ObservableObject {
     @Published var currentArtist: String
     @Published var currentArtwork: Data?
     @Published var currentURL: URL?
-
+    
+    
     init(metadata: Metadata) {
         currentTitle = metadata.title ?? ""
         currentArtist = metadata.artist ?? ""
@@ -69,5 +102,7 @@ final class EditorViewModel: ObservableObject {
         currentArtwork = metadata.artwork
         currentURL = metadata.url
     }
+    
+    
     
 }
